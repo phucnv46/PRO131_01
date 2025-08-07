06242254d9dfe2e1e0d8af5e6c80e4151be628b9
phucnv46 <phucnv46@fpt.edu.vn>
2025-08-07 12:06:43 +0700

    Upload

---
+++ Models/HoaDonChiTiet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRO131_01.Models;
+
+public partial class HoaDonChiTiet
+{
+    public string MaHoaDon { get; set; } = null!;
+
+    public string MaSanPham { get; set; } = null!;
+
+    public int SoLuong { get; set; }
+
+    public decimal DonGia { get; set; }
+
+    public virtual HoaDon MaHoaDonNavigation { get; set; } = null!;
+
+    public virtual SanPham MaSanPhamNavigation { get; set; } = null!;
+}
